5381e60f883ed80f2310497fdf5cd97290d8eee1
Grzegorz Haruk <g.haruk@godeltech.com>
2022-10-11 16:03:04 +0200

    Add initial project structure

---
+++ ApiInChains/ApiInChains.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApiInChains.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
